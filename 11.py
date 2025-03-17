e7d0fc3c514055ca9f1a47b36811bd14d9d0cd64
ZasX <zasxxsaz@gmail.com>
2025-03-18 00:58:30 +0100

    day 11 small optimization

--- 11.py
+++ 11.py
@@ -3,7 +3,7 @@ from utils.runner import run_puzzle
 
 UPPERGREEK = "ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ"
 LOWERGREEK = "αβγδεζηθικλμνξοπρστυφχψω"
-ODYSEUS = ("Οδυσσευς","Οδυσσεως","Οδυσσει","Οδυσσεα","Οδυσσευ")
+ODYSEUS = ("Οδυσσευσ","Οδυσσεωσ","Οδυσσει","Οδυσσεα","Οδυσσευ")
 
 def rotate(s: str, p: int):
     result = ""
@@ -18,15 +18,19 @@ def rotate(s: str, p: int):
             result += c
     return result
 
+def odyseus_in_rot(line: str):
+    for i in range(24):
+        newline = rotate(line, i)
+        for o in ODYSEUS:
+            if o in newline:
+                return i
+    return 0
+
 def solve(puzzle_input: str) -> int:
     answer = 0
     puzzle_input = puzzle_input.replace('ς', 'σ')
     for line in puzzle_input.splitlines():
-        for i in range(24):
-            newline = rotate(line, i)
-            for o in ODYSEUS:
-                if o in newline:
-                    answer += i
+        answer += odyseus_in_rot(line)
     return answer
 
 if __name__ == "__main__":
